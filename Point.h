2a2ec8eee76a193304e26d4ae28902863ff4e416
AIDANCAUGHEY <>
2022-03-09 17:12:52 +0000

    Worksheet 1

--- Point.h
+++ Point.h
@@ -1,6 +1,15 @@
 class Point{
     public:
         Point();
+        Point(double xVal, double yVal);
+        //Accesors
+        /*double getX();
+        double getY();
+        //Mutators
+        void setX(double xVal);
+        void setY(double yVal);*/
+        void print();
+        double calcDistance(Point other);
     private:
         double x;
         double y;
