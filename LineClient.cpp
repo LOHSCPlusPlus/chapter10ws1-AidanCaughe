c2e07c16a01f5be7032a67c24af1eaf28217b016
AIDANCAUGHEY <>
2022-03-09 20:53:25 +0000

    WS 2 Done

--- LineClient.cpp
+++ LineClient.cpp
@@ -1,5 +1,12 @@
 #include "Line.h"
-
+#include <iostream>
+using namespace std;
 int main() {
+  Point a(3.0,2.0);
+  Point b(5.4,2.7);
+  Line l(a,b);
+  l.print();
+  cout << l.lineLength() << endl;
     return 0;
+  
 }
